489cfcad1a1d23861160c3b725dcd2a4f74911a2
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2023-03-10 13:06:23 +0330

    feat: Complete custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,13 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,\
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+    def create_user(self, email, password=None, commit=True, **extra_fields):
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address.')
+
         if password:
             user.set_password(password)
+
+        if commit:
+            user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password, commit=False)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,3 +18,25 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@HOSSEINDEV.com'
+        user = User.objects.create_user(email=email, password='123123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            User.objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new super user"""
+        user = User.objects.create_superuser(
+            email='test@hosseindev.com',
+            password='test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertEqual(user.email, 'test@hosseindev.com')
